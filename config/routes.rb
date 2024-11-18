f26f10cbb2b7a26856c9b4b05109c079f1b59d7f
Ryookkey <Ryookkey@users.noreply.github.com>
2024-11-18 12:29:25 +0900

    一覧画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "lists/show"
   get "lists/edit"
   get '/top' => 'homes#top'
+  get 'lists' => 'lists#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

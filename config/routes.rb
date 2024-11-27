fbf8ff1356ff6a8370966aecc592bb0d79cf5fef
Ryookkey <Ryookkey@users.noreply.github.com>
2024-11-27 18:04:47 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,4 @@
 Rails.application.routes.draw do
-  get "lists/new"
-  get "lists/:id/edit" => 'lists#edit', as: 'edit_list'
-  get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  patch 'lists/:id' => 'lists#update' , as: 'update_list'
-  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'top' => 'homes#top'
+  resources :lists
 end
